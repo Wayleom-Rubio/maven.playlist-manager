64705f0abc07870608efef11911da755e5b0a879
wrvargas20students.desu.edu <wrvargas20students.desu.edu>
2023-05-04 09:22:17 -0400

    I made changes

--- src/main/java/com/github/curriculeon/Playlist.java
+++ src/main/java/com/github/curriculeon/Playlist.java
@@ -4,18 +4,26 @@ package com.github.curriculeon;
  * Created by leon on 1/10/2021.
  */
 public class Playlist {
+
+    private int currentSong;
+    private final String[] playlist;
+
     public Playlist(String[] songNameArray) {
+        this(0,songNameArray);
+
     }
 
     public Playlist(int currentIndex, String[] songNameArray) {
+        currentSong = currentIndex;
+        playlist = songNameArray;
     }
 
     public String[] getSongNameArray() {
-        return null;
+        return playlist;
     }
 
     public String getCurrentSelection() {
-        return null;
+        return getSongNameArray()[currentSong];
     }
 
     public void goToPreviousSong() {
