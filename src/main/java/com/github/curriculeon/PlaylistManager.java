6177655deea0704e8f359c6568fd4c2c1fe48bb1
wrvargas20students.desu.edu <wrvargas20students.desu.edu>
2023-05-04 13:34:27 -0400

    I made changes

--- src/main/java/com/github/curriculeon/PlaylistManager.java
+++ src/main/java/com/github/curriculeon/PlaylistManager.java
@@ -20,16 +20,23 @@ public class PlaylistManager {
     }
 
     public Playlist getPlaylist() {
-        return null;
+        return playlist;
     }
 
     public void setPlaylist(Playlist playlist) {
+        if(playlist != null) this.playlist = playlist;
+        else throw new IllegalArgumentException();
     }
 
     private void setPlaylist(String[] toArray) {
+        setPlaylist(new Playlist(toArray));
     }
 
     public void addSong(String songToAdd) {
+        String[] track = new String[playlist.getSongNameArray().length + 1];
+        System.arraycopy(playlist.getSongNameArray(),0, track, 0, playlist.getSongNameArray().length);
+        track[playlist.getSongNameArray().length] = songToAdd;
+        setPlaylist(track);
     }
 
     public void removeSong(String songToRemove) {
